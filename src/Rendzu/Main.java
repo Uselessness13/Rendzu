335bccd5c90918274cf74ff395cd201b71bce93e
UserTundra <haizbq@gmail.com>
2017-05-15 20:40:17 +0400

    i did it

--- src/Rendzu/Main.java
+++ src/Rendzu/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1024, 768));
+        Parent root = FXMLLoader.load(getClass().getResource("mainScene.fxml"));
+        primaryStage.setTitle("Rendju");
+        primaryStage.setScene(new Scene(root, 1030, 623));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
